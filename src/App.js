9dcdd021fc07d1f93f9465482629cc321351536b
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-09-17 15:39:41 +0200

    Final build

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Lottery from './Lottery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div><Lottery /></div>
+        <div><Lottery title={"Mini lotto"} maxNum={10} numBalls={4} /></div>
+      </div>
+    );
+  }
 }
 
 export default App;
